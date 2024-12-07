9fc349b32dcf8f7e0fd2a3946478e41297cada88
Hayaka <erica223020@gmail.com>
2024-12-08 02:13:35 +0800

    新增題

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -106,10 +106,44 @@ values
     -- 2. 將用戶`肌肉棒子`新增為教練，並且年資設定為2年
     -- 3. 將用戶`Q太郎`新增為教練，並且年資設定為2年
 
+insert into "COACH" 
+(user_id,experience_years)
+values 
+	((select id from "USER" where email = 'lee2000@hexschooltest.io'),2),
+	((select id from "USER" where email = 'muscle@hexschooltest.io'),2),
+	((select id from "USER" where email = 'starplatinum@hexschooltest.io'),2);
+
 -- 3-2. 新增：承1，為三名教練新增專長資料至 `COACH_LINK_SKILL` ，資料需求如下：
     -- 1. 所有教練都有 `重訓` 專長
     -- 2. 教練`肌肉棒子` 需要有 `瑜伽` 專長
     -- 3. 教練`Q太郎` 需要有 `有氧運動` 與 `復健訓練` 專長
+
+INSERT INTO "COACH_LINK_SKILL" (coach_id,skill_id)
+VALUES 
+(
+    (SELECT id FROM "COACH" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'lee2000@hexschooltest.io')),
+    (SELECT id FROM "SKILL" WHERE name = '重訓')
+),
+(
+    (SELECT id FROM "COACH" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'muscle@hexschooltest.io')),
+    (SELECT id FROM "SKILL" WHERE name = '重訓')
+),
+(
+    (SELECT id FROM "COACH" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'muscle@hexschooltest.io')),
+    (SELECT id FROM "SKILL" WHERE name = '瑜伽')
+),
+(
+    (SELECT id FROM "COACH" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'starplatinum@hexschooltest.io')),
+    (SELECT id FROM "SKILL" WHERE name = '重訓')
+),
+(
+    (SELECT id FROM "COACH" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'starplatinum@hexschooltest.io')),
+    (SELECT id FROM "SKILL" WHERE name = '有氧運動')
+),
+(
+    (SELECT id FROM "COACH" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'starplatinum@hexschooltest.io')),
+    (SELECT id FROM "SKILL" WHERE name = '復健訓練')
+);
 
 -- 3-3 修改：更新教練的經驗年數，資料需求如下：
     -- 1. 教練`肌肉棒子` 的經驗年數為3年
